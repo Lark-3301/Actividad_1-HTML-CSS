29cac9d44ebefef7659babf089a692bcd4bd89bf
vicmac-github <69369181+Lark-3301@users.noreply.github.com>
2025-10-12 11:36:30 +0200

    He añadido la funcionalidad de sumar y reset a opiniones.html

--- opiniones-js.js
+++ opiniones-js.js
@@ -4,3 +4,18 @@ console.log(btnAlert)
 btnAlert.addEventListener('click',()=>{
     alert('Alerta Camaradas')
 })
+
+contador = 0; 
+let pantallaContador = document.getElementById('counter');
+let btnSumar = document.getElementById('btn-sumar');
+let btnReset =document.getElementById('btn-reset');
+
+btnSumar.addEventListener('click', () => {
+    contador ++;
+    pantallaContador.textContent = contador;
+})
+
+btnReset.addEventListener('click', () => {
+    contador = 0;
+    pantallaContador.textContent = contador;
+})
